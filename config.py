291287bc20fa7c0c81e03aa77986580a5e7dac9e
Bairon Lozano <baironlozano334@gmail.com>
2026-03-12 14:41:16 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 class Config:
     SECRET_KEY = 'royal-spin-clave-secreta-2025-casino'
     SESSION_TYPE = 'filesystem'
-    MYSQL_HOST     = 'caboose.proxy.rlwy.net'
-    MYSQL_USER     = 'root'
-    MYSQL_PASSWORD = 'kQpUzDUtmOyxPOvOncNhkpmushuyenZO'
-    MYSQL_DB       = 'railway'
-    MYSQL_PORT     = 51598
+    MYSQL_HOST     = 'saisvq.h.filess.io'
+    MYSQL_USER     = 'casino_cropnineam'
+    MYSQL_PASSWORD = '86369322b63eb4a15a50c59320ba72aee2d2e77d'
+    MYSQL_DB       = 'casino_cropnineam'
+    MYSQL_PORT     = 3306
     MAIL_CONFIG = {
         'remitente': 'baironlozano334@gmail.com',
         'password':  'kujtklomjsawbxox',
