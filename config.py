4fbe9360eaf848e5222539dffe367e338dcca182
Bairon Lozano <baironlozano334@gmail.com>
2026-03-12 13:40:38 -0500

    conectar base de datos filess

--- config.py
+++ config.py
@@ -4,14 +4,12 @@ class Config:
     SECRET_KEY = 'royal-spin-clave-secreta-2025-casino'
     SESSION_TYPE = 'filesystem'
 
-    # ─── CONFIGURACIÓN MYSQL ────────────────────────────────────────
-    # *** CAMBIA ESTOS DATOS POR LOS DE TU SERVIDOR MYSQL ***
-    MYSQL_HOST     = 'localhost'
-    MYSQL_USER     = 'root'          # <-- tu usuario MySQL
-    MYSQL_PASSWORD = '0000'              # <-- tu contraseña MySQL
-    MYSQL_DB       = 'casino'        # <-- nombre de la base de datos
+    MYSQL_HOST     = 'saisvq.h.filess.io'
+    MYSQL_USER     = 'casino_cropnineam'
+    MYSQL_PASSWORD = '86369322b63eb4a15a50c59320ba72aee2d2e77d'
+    MYSQL_DB       = 'casino_cropnineam'
+    MYSQL_PORT     = 3306
 
-    # ─── CONFIGURACIÓN DE CORREO ────────────────────────────────────
     MAIL_CONFIG = {
         'remitente': 'baironlozano334@gmail.com',
         'password':  'kujtklomjsawbxox',
